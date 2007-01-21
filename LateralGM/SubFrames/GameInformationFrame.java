8a884653a777ed15f298e445778ce482744cbbc8
tgmg <tgmg@8f422083-7f27-0410-bc82-93e204be8cd2>
2007-01-21 12:12:00 +0000

    git-svn-id: https://lateralgm.svn.sourceforge.net/svnroot/lateralgm@12 8f422083-7f27-0410-bc82-93e204be8cd2

--- LateralGM/SubFrames/GameInformationFrame.java
+++ LateralGM/SubFrames/GameInformationFrame.java
@@ -1,17 +1,30 @@
 package SubFrames;
 
+import java.awt.BorderLayout;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.rtf.RTFEditorKit;
+
+import resourcesRes.GameInformation;
 
 import mainRes.LGM;
 
-public class GameInformation extends JInternalFrame
+public class GameInformationFrame extends JInternalFrame
 	{
-	public GameInformation()
+	private JEditorPane editor;
+
+	public GameInformationFrame()
 		{
 		super("Game Information",true,true,true,true);
 
@@ -65,6 +78,33 @@ public class GameInformation extends JInternalFrame
 		but.setActionCommand("Save");
 		// but.addActionListener(listener);
 		tool.add(but);
-		
+
+		// Create an RTF editor window
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BorderLayout());
+		getContentPane().add(topPanel,BorderLayout.CENTER);
+		RTFEditorKit rtf = new RTFEditorKit();
+		editor = new JEditorPane();
+		editor.setEditable(false);
+		editor.setEditorKit(rtf);
+		// editor.setBackground(Color.);
+
+		// This text could be big so add a scroll pane
+		JScrollPane scroller = new JScrollPane();
+		scroller.getViewport().add(editor);
+		topPanel.add(scroller,BorderLayout.CENTER);
+
+		GameInformation gi = new GameInformation();
+		try
+			{
+			rtf.read(new ByteArrayInputStream(gi.GameInfoStr.getBytes()),editor.getDocument(),0);
+			}
+		catch (Exception e)
+			{
+			e.printStackTrace();
+			}
+
+		// gi.GameInfoStr;
+
 		}
 	}
--- LateralGM/mainRes/LGM.java
+++ LateralGM/mainRes/LGM.java
@@ -157,7 +157,7 @@ public class LGM extends JPanel
 		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,true,scroll,scroll2);
 		split.setDividerLocation(170);
 		add(split);
-		JInternalFrame welcome = new GameInformation();// new JInternalFrame("Welcome to
+		JInternalFrame welcome = new GameInformationFrame();// new JInternalFrame("Welcome to
 		// LGM!",true,true,true,true);
 		welcome.setVisible(true);
 		welcome.setSize(300,300);
