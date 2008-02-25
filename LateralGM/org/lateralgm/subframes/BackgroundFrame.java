d118b3073330262eb82cf1b3c793b3f46bb50fd6
quadduc <quadduc@8f422083-7f27-0410-bc82-93e204be8cd2>
2008-02-25 07:56:42 +0000

    Fixed layout in `BackgroundFrame` using `GroupLayout` manager, and improved layout of some `SpriteFrame` components (see also r229).
    git-svn-id: https://lateralgm.svn.sourceforge.net/svnroot/lateralgm@230 8f422083-7f27-0410-bc82-93e204be8cd2

--- LateralGM/org/lateralgm/subframes/BackgroundFrame.java
+++ LateralGM/org/lateralgm/subframes/BackgroundFrame.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 Clam <ebordin@aapt.net.au>
+ * Copyright (C) 2008 Quadduc <quadduc@gmail.com>
  * 
  * This file is part of Lateral GM. Lateral GM is free
  * software and comes with ABSOLUTELY NO WARRANTY.
@@ -8,15 +9,17 @@
 
 package org.lateralgm.subframes;
 
-import static org.lateralgm.main.Util.addDim;
+import static java.lang.Integer.MAX_VALUE;
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+import static javax.swing.GroupLayout.PREFERRED_SIZE;
 
+import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -25,6 +28,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
 
 import org.lateralgm.compare.ResourceComparator;
 import org.lateralgm.components.IntegerField;
@@ -61,125 +65,195 @@ public class BackgroundFrame extends ResourceFrame<Background>
 	public BackgroundFrame(Background res, ResNode node)
 		{
 		super(res,node);
+		GroupLayout layout = new GroupLayout(getContentPane())
+			{
+				@Override
+				public void layoutContainer(Container parent)
+					{
+					Dimension m = BackgroundFrame.this.getMinimumSize();
+					Dimension s = BackgroundFrame.this.getSize();
+					Dimension r = new Dimension(Math.max(m.width,s.width),Math.max(m.height,s.height));
+					if (!r.equals(s))
+						BackgroundFrame.this.setSize(r);
+					else
+						super.layoutContainer(parent);
+					}
+			};
+		setLayout(layout);
 
-		setSize(560,320);
-		setMinimumSize(new Dimension(450,320));
-		setLayout(new BoxLayout(getContentPane(),BoxLayout.X_AXIS));
+		JPanel side1 = new JPanel();
+		makeSide1(side1);
+		side2 = new JPanel();
+		makeSide2(side2);
 
-		JPanel side1 = new JPanel(new FlowLayout());
-		side1.setMinimumSize(new Dimension(180,280));
-		side1.setMaximumSize(new Dimension(180,Integer.MAX_VALUE));
-		side1.setPreferredSize(new Dimension(180,280));
-
-		// side1.setBackground(Color.RED);
-		Util.addDim(side1,new JLabel(Messages.getString("BackgroundFrame.NAME")),40,14); //$NON-NLS-1$
-		addDim(side1,name,120,20);
-
-		load = new JButton(Messages.getString("SpriteFrame.LOAD")); //$NON-NLS-1$
-		load.setIcon(LOAD_ICON);
-		load.addActionListener(this);
-		addDim(side1,load,130,24);
-		width = new JLabel(Messages.getString("BackgroundFrame.WIDTH") + res.width); //$NON-NLS-1$
-		addDim(side1,width,80,16);
-		height = new JLabel(Messages.getString("BackgroundFrame.HEIGHT") + res.height); //$NON-NLS-1$
-		addDim(side1,height,80,16);
-
-		addGap(side1,160,10);
-
-		edit = new JButton(Messages.getString("BackgroundFrame.EDIT")); //$NON-NLS-1$
-		edit.addActionListener(this);
-		addDim(side1,edit,130,24);
-
-		addGap(side1,160,15);
-
-		transparent = new JCheckBox(Messages.getString("BackgroundFrame.TRANSPARENT")); //$NON-NLS-1$
-		transparent.setSelected(res.transparent);
-		transparent.addActionListener(this);
-		addDim(side1,transparent,130,16);
-		smooth = new JCheckBox(Messages.getString("BackgroundFrame.SMOOTH")); //$NON-NLS-1$
-		smooth.setSelected(res.smoothEdges);
-		addDim(side1,smooth,130,16);
-		preload = new JCheckBox(Messages.getString("BackgroundFrame.PRELOAD")); //$NON-NLS-1$
-		preload.setSelected(res.preload);
-		addDim(side1,preload,130,16);
-		tileset = new JCheckBox(Messages.getString("BackgroundFrame.USE_AS_TILESET")); //$NON-NLS-1$
-		tileset.setSelected(res.useAsTileSet);
-		tileset.addActionListener(this);
-		addDim(side1,tileset,130,16);
-
-		addGap(side1,160,15);
-
-		save.setText(Messages.getString("BackgroundFrame.SAVE")); //$NON-NLS-1$
-		addDim(side1,save,130,24);
-
-		side2 = new JPanel(new FlowLayout());
-		side2.setPreferredSize(new Dimension(180,260));
-		side2.setMinimumSize(new Dimension(180,260));
-		side2.setMaximumSize(new Dimension(180,Integer.MAX_VALUE));
-		JPanel group = new JPanel(new FlowLayout()); // BoxLayout does what it wants, so this has to be
-		// separate to stay a constant size
-		group.setPreferredSize(new Dimension(180,270));
-		String tileProps = Messages.getString("BackgroundFrame.TILE_PROPERTIES"); //$NON-NLS-1$
-		group.setBorder(BorderFactory.createTitledBorder(tileProps));
-
-		JLabel lab = new JLabel(Messages.getString("BackgroundFrame.TILE_WIDTH")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		tWidth = new IntegerField(0,Integer.MAX_VALUE,res.tileWidth);
-		tWidth.addActionListener(this);
-		addDim(group,tWidth,50,20);
-
-		lab = new JLabel(Messages.getString("BackgroundFrame.TILE_HEIGHT")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		tHeight = new IntegerField(0,Integer.MAX_VALUE,res.tileHeight);
-		tHeight.addActionListener(this);
-		addDim(group,tHeight,50,20);
-
-		addGap(group,150,15);
-
-		lab = new JLabel(Messages.getString("BackgroundFrame.H_OFFSET")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		hOffset = new IntegerField(0,Integer.MAX_VALUE,res.horizOffset);
-		hOffset.addActionListener(this);
-		addDim(group,hOffset,50,20);
-
-		lab = new JLabel(Messages.getString("BackgroundFrame.V_OFFSET")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		vOffset = new IntegerField(0,Integer.MAX_VALUE,res.vertOffset);
-		vOffset.addActionListener(this);
-		addDim(group,vOffset,50,20);
-
-		addGap(group,150,15);
-
-		lab = new JLabel(Messages.getString("BackgroundFrame.H_SEP")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		hSep = new IntegerField(0,Integer.MAX_VALUE,res.horizSep);
-		hSep.addActionListener(this);
-		addDim(group,hSep,50,20);
-
-		lab = new JLabel(Messages.getString("BackgroundFrame.V_SEP")); //$NON-NLS-1$
-		lab.setHorizontalAlignment(SwingConstants.RIGHT);
-		addDim(group,lab,100,16);
-		vSep = new IntegerField(0,Integer.MAX_VALUE,res.vertSep);
-		vSep.addActionListener(this);
-		addDim(group,vSep,50,20);
-
-		side2.add(group);
-		side2.setVisible(tileset.isSelected());
-
-		add(side1);
-		add(side2);
 		preview = new BackgroundPreview(this);
 		if (res.backgroundImage != null)
 			preview.setIcon(new ImageIcon(res.backgroundImage));
 		else
 			preview.setPreferredSize(new Dimension(0,0));
 		preview.setVerticalAlignment(SwingConstants.TOP);
-		add(new JScrollPane(preview));
+		JScrollPane scroll = new JScrollPane(preview);
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+		/**/.addComponent(side1,DEFAULT_SIZE,PREFERRED_SIZE,PREFERRED_SIZE)
+		/**/.addComponent(side2,DEFAULT_SIZE,PREFERRED_SIZE,PREFERRED_SIZE)
+		/**/.addComponent(scroll,120,320,DEFAULT_SIZE));
+		layout.setVerticalGroup(layout.createParallelGroup()
+		/**/.addComponent(side1)
+		/**/.addComponent(side2)
+		/**/.addComponent(scroll,120,240,DEFAULT_SIZE));
+		pack();
+		}
+
+	private void makeSide1(JPanel side1)
+		{
+		GroupLayout s1Layout = new GroupLayout(side1);
+		s1Layout.setAutoCreateContainerGaps(true);
+		s1Layout.setAutoCreateGaps(true);
+		side1.setLayout(s1Layout);
+
+		JLabel nLabel = new JLabel(Messages.getString("BackgroundFrame.NAME"));
+		load = new JButton(Messages.getString("SpriteFrame.LOAD")); //$NON-NLS-1$
+		load.setIcon(LOAD_ICON);
+		load.addActionListener(this);
+		width = new JLabel(Messages.getString("BackgroundFrame.WIDTH") + res.width); //$NON-NLS-1$
+		height = new JLabel(Messages.getString("BackgroundFrame.HEIGHT") + res.height); //$NON-NLS-1$
+
+		edit = new JButton(Messages.getString("BackgroundFrame.EDIT")); //$NON-NLS-1$
+		edit.addActionListener(this);
+
+		transparent = new JCheckBox(Messages.getString("BackgroundFrame.TRANSPARENT")); //$NON-NLS-1$
+		transparent.setSelected(res.transparent);
+		transparent.addActionListener(this);
+		smooth = new JCheckBox(Messages.getString("BackgroundFrame.SMOOTH")); //$NON-NLS-1$
+		smooth.setSelected(res.smoothEdges);
+		preload = new JCheckBox(Messages.getString("BackgroundFrame.PRELOAD")); //$NON-NLS-1$
+		preload.setSelected(res.preload);
+		tileset = new JCheckBox(Messages.getString("BackgroundFrame.USE_AS_TILESET")); //$NON-NLS-1$
+		tileset.setSelected(res.useAsTileSet);
+		tileset.addActionListener(this);
+
+		save.setText(Messages.getString("BackgroundFrame.SAVE")); //$NON-NLS-1$
+
+		s1Layout.setHorizontalGroup(s1Layout.createParallelGroup()
+		/**/.addGroup(s1Layout.createSequentialGroup()
+		/*		*/.addComponent(nLabel)
+		/*		*/.addComponent(name,DEFAULT_SIZE,120,MAX_VALUE))
+		/**/.addComponent(load,DEFAULT_SIZE,DEFAULT_SIZE,MAX_VALUE)
+		/**/.addGroup(s1Layout.createSequentialGroup()
+		/*		*/.addComponent(width)
+		/*		*/.addComponent(height))
+		/**/.addComponent(edit,DEFAULT_SIZE,DEFAULT_SIZE,MAX_VALUE)
+		/**/.addComponent(transparent)
+		/**/.addComponent(smooth)
+		/**/.addComponent(preload)
+		/**/.addComponent(tileset)
+		/**/.addComponent(save,DEFAULT_SIZE,DEFAULT_SIZE,MAX_VALUE));
+		s1Layout.setVerticalGroup(s1Layout.createSequentialGroup()
+		/**/.addGroup(s1Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(nLabel)
+		/*		*/.addComponent(name))
+		/**/.addComponent(load)
+		/**/.addGroup(s1Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(width)
+		/*		*/.addComponent(height))
+		/**/.addComponent(edit)
+		/**/.addComponent(transparent)
+		/**/.addComponent(smooth)
+		/**/.addComponent(preload)
+		/**/.addComponent(tileset)
+		/**/.addGap(8,8,MAX_VALUE)
+		/**/.addComponent(save));
+		}
+
+	private void makeSide2(JPanel side2)
+		{
+		GroupLayout s2Layout = new GroupLayout(side2);
+		side2.setLayout(s2Layout);
+		String tileProps = Messages.getString("BackgroundFrame.TILE_PROPERTIES"); //$NON-NLS-1$
+		side2.setBorder(BorderFactory.createTitledBorder(tileProps));
+
+		JLabel twLabel = new JLabel(Messages.getString("BackgroundFrame.TILE_WIDTH")); //$NON-NLS-1$
+		twLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		tWidth = new IntegerField(0,Integer.MAX_VALUE,res.tileWidth);
+		tWidth.addActionListener(this);
+		tWidth.setColumns(3);
+
+		JLabel thLabel = new JLabel(Messages.getString("BackgroundFrame.TILE_HEIGHT")); //$NON-NLS-1$
+		thLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		tHeight = new IntegerField(0,Integer.MAX_VALUE,res.tileHeight);
+		tHeight.addActionListener(this);
+		tHeight.setColumns(3);
+
+		JLabel hoLabel = new JLabel(Messages.getString("BackgroundFrame.H_OFFSET")); //$NON-NLS-1$
+		hoLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		hOffset = new IntegerField(0,Integer.MAX_VALUE,res.horizOffset);
+		hOffset.addActionListener(this);
+		hOffset.setColumns(3);
+
+		JLabel voLabel = new JLabel(Messages.getString("BackgroundFrame.V_OFFSET")); //$NON-NLS-1$
+		voLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		vOffset = new IntegerField(0,Integer.MAX_VALUE,res.vertOffset);
+		vOffset.addActionListener(this);
+		vOffset.setColumns(3);
+
+		JLabel hsLabel = new JLabel(Messages.getString("BackgroundFrame.H_SEP")); //$NON-NLS-1$
+		hsLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		hSep = new IntegerField(0,Integer.MAX_VALUE,res.horizSep);
+		hSep.addActionListener(this);
+		hSep.setColumns(3);
+
+		JLabel vsLabel = new JLabel(Messages.getString("BackgroundFrame.V_SEP")); //$NON-NLS-1$
+		vsLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		vSep = new IntegerField(0,Integer.MAX_VALUE,res.vertSep);
+		vSep.addActionListener(this);
+		vSep.setColumns(3);
+
+		s2Layout.setHorizontalGroup(s2Layout.createSequentialGroup()
+		/**/.addContainerGap(4,4)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.TRAILING)
+		/*		*/.addComponent(twLabel)
+		/*		*/.addComponent(thLabel)
+		/*		*/.addComponent(hoLabel)
+		/*		*/.addComponent(voLabel)
+		/*		*/.addComponent(hsLabel)
+		/*		*/.addComponent(vsLabel))
+		/**/.addGap(4)
+		/**/.addGroup(s2Layout.createParallelGroup()
+		/*		*/.addComponent(tWidth,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
+		/*		*/.addComponent(tHeight,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
+		/*		*/.addComponent(hOffset,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
+		/*		*/.addComponent(vOffset,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
+		/*		*/.addComponent(hSep,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE)
+		/*		*/.addComponent(vSep,PREFERRED_SIZE,DEFAULT_SIZE,DEFAULT_SIZE))
+		/**/.addContainerGap(4,4));
+		s2Layout.setVerticalGroup(s2Layout.createSequentialGroup()
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(twLabel)
+		/*		*/.addComponent(tWidth))
+		/**/.addGap(2)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(thLabel)
+		/*		*/.addComponent(tHeight))
+		/**/.addGap(8)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(hoLabel)
+		/*		*/.addComponent(hOffset))
+		/**/.addGap(2)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(voLabel)
+		/*		*/.addComponent(vOffset))
+		/**/.addGap(8)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(hsLabel)
+		/*		*/.addComponent(hSep))
+		/**/.addGap(2)
+		/**/.addGroup(s2Layout.createParallelGroup(Alignment.BASELINE)
+		/*		*/.addComponent(vsLabel)
+		/*		*/.addComponent(vSep))
+		/**/.addContainerGap(8,8));
+
+		side2.setVisible(tileset.isSelected());
 		}
 
 	@Override
@@ -254,5 +328,16 @@ public class BackgroundFrame extends ResourceFrame<Background>
 			}
 
 		super.actionPerformed(e);
+		}
+
+	@Override
+	public Dimension getMinimumSize()
+		{
+		Dimension p = getContentPane().getSize();
+		Dimension l = getContentPane().getMinimumSize();
+		Dimension s = getSize();
+		l.width += s.width - p.width;
+		l.height += s.height - p.height;
+		return l;
 		}
 	}
