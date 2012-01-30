0f9fdd3a2646a6d3c8aa00377320b013b8bbbdce
Medo <smaxein@googlemail.com>
2012-01-30 23:48:10 +0100

    Re-added the spatial bin optimization, but much more concisely
    The linear implementation turned out to be just a tad too slow for my
    liking: In a room containing 10000 tiles (roughly the limit of what GM
    can handle at 30FPS in a running game on my PC), pasting new tiles
    continuously for testing, the intersection test was the second most CPU
    intensive method (behind some Swing color update code that seems to be
    run for a large list of UI elements).
    
    This was still usable, but worse than I had expected from my initial
    testing, so I decided to add back the bin optimization after all, though
    in a less thorough (but far shorter and imo more readable) form.

--- org/lateralgm/ui/swing/visuals/BinVisual.java
+++ org/lateralgm/ui/swing/visuals/BinVisual.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2012 Medo <smaxein@googlemail.com> 
  * Copyright (C) 2009 Quadduc <quadduc@gmail.com>
  * 
  * This file is part of LateralGM.
@@ -9,60 +10,24 @@
 package org.lateralgm.ui.swing.visuals;
 
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.Set;
 
 public class BinVisual extends AbstractVisual implements VisualContainer,BoundedVisual
 	{
-	public static class MockVisual implements Visual
-		{
-		public void paint(Graphics g)
-			{
-			}
-		}
-
-	private static class VisualItem
-		{
-		final Visual visual;
-		final Rectangle bounds;
-		final int depth;
-	
-		public VisualItem(Visual visual, Rectangle bounds, int depth)
-			{
-			this.visual = visual;
-			if (bounds == null)
-				this.bounds = null;
-			else
-				{
-				this.bounds = new Rectangle(bounds);
-				}
-			this.depth = depth;
-			}
-		}
-
-	private static class InverseDepthComparator implements Comparator<VisualItem>
-		{
-		public int compare(VisualItem o1, VisualItem o2)
-			{
-			if (o1.depth > o2.depth)
-				return -1;
-			else if (o1.depth < o2.depth)
-				return 1;
-			else
-				return System.identityHashCode(o1) - System.identityHashCode(o2);
-			}
-		}
-
-	private final Map<Visual,VisualItem> itemMap = new HashMap<Visual,BinVisual.VisualItem>();
-	private final SortedSet<VisualItem> depthSortedItems = new TreeSet<VisualItem>(
-			new InverseDepthComparator());
+	private final Map<Visual,VisualItem> itemMap = new HashMap<Visual,VisualItem>();
+	private final SpatialHashMap spatialHashMap = new SpatialHashMap();
 	private Rectangle overallBounds = new Rectangle();
 
 	public BinVisual(VisualContainer c)
@@ -70,21 +35,13 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 		super(c);
 		}
 
-	@Override
-	public void repaint(Rectangle r)
-		{
-		if (parent != null)
-			super.repaint(r);
-		}
-
 	public void add(Visual v, Rectangle b, int d)
 		{
 		VisualItem item = new VisualItem(v,b,d);
 		VisualItem previous = itemMap.put(v,item);
 		if (previous != null)
-			depthSortedItems.remove(previous);
-		if (b != null)
-			depthSortedItems.add(item);
+			spatialHashMap.remove(previous);
+		spatialHashMap.put(item);
 
 		if (item.bounds != null)
 			repaint(item.bounds);
@@ -100,7 +57,7 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 		{
 		VisualItem item = itemMap.remove(v);
 		if (item == null) return false;
-		depthSortedItems.remove(item);
+		spatialHashMap.remove(item);
 		if (item.bounds != null)
 			{
 			repaint(item.bounds);
@@ -126,9 +83,8 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 	public Iterator<Visual> intersect(Rectangle r)
 		{
 		List<Visual> result = new ArrayList<Visual>();
-		for (VisualItem item : depthSortedItems)
-			if (r.intersects(item.bounds))
-				result.add(item.visual);
+		for (VisualItem item : spatialHashMap.intersect(r))
+			result.add(item.visual);
 		return result.iterator();
 		}
 
@@ -136,8 +92,8 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 	public <V extends Visual>Iterator<V> intersect(Rectangle r, final Class<V> v)
 		{
 		List<V> result = new ArrayList<V>();
-		for (VisualItem item : depthSortedItems)
-			if (v.isInstance(item.visual) && r.intersects(item.bounds))
+		for (VisualItem item : spatialHashMap.intersect(r))
+			if (v.isInstance(item.visual))
 				result.add((V) item.visual);
 		return result.iterator();
 		}
@@ -145,23 +101,21 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 	public void paint(Graphics g)
 		{
 		Rectangle clip = g.getClipBounds();
-		for (VisualItem item : depthSortedItems)
+		List<VisualItem> items = spatialHashMap.intersect(clip);
+		Collections.sort(items,new InverseDepthComparator());
+		for (VisualItem item : items)
 			{
 			Rectangle b = item.bounds;
-			if (b.intersects(clip))
-				{
-				Graphics g2 = g.create(b.x,b.y,b.width,b.height);
-				item.visual.paint(g2);
-				g2.dispose();
-				}
+			Graphics g2 = g.create(b.x,b.y,b.width,b.height);
+			item.visual.paint(g2);
+			g2.dispose();
 			}
 		}
 
 	public void invalidateBounds()
 		{
 		overallBounds = null;
-		if (parent != null)
-			parent.updateBounds();
+		parent.updateBounds();
 		}
 
 	public void updateBounds()
@@ -174,10 +128,17 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 		if (overallBounds == null)
 			{
 			overallBounds = new Rectangle();
-			for (VisualItem item : depthSortedItems)
-				overallBounds.add(item.bounds);
+			for (VisualItem item : itemMap.values())
+				if (item.bounds != null)
+					overallBounds.add(item.bounds);
 			}
 		b.add(overallBounds);
+		}
+
+	@Override
+	public void repaint(Rectangle r)
+		{
+			super.repaint(r);
 		}
 
 	private boolean equalRect(Rectangle r1, Rectangle r2)
@@ -194,5 +155,148 @@ public class BinVisual extends AbstractVisual implements VisualContainer,Bounded
 		Rectangle b1 = i1 == null ? null : i1.bounds;
 		Rectangle b2 = i2 == null ? null : i2.bounds;
 		return equalRect(b1,b2);
+		}
+
+	private static class VisualItem
+		{
+		final Visual visual;
+		// bounds is intended immutable, and SpatialHashMap relies on that - don't change it!
+		final Rectangle bounds;
+		final int depth;
+
+		public VisualItem(Visual visual, Rectangle bounds, int depth)
+			{
+			this.visual = visual;
+			this.bounds = bounds == null ? null : new Rectangle(bounds);
+			this.depth = depth;
+			}
+		}
+
+	private static class InverseDepthComparator implements Comparator<VisualItem>
+		{
+		public int compare(VisualItem o1, VisualItem o2)
+			{
+			if (o1.depth > o2.depth)
+				return -1;
+			else if (o1.depth < o2.depth)
+				return 1;
+			else
+				return System.identityHashCode(o1) - System.identityHashCode(o2);
+			}
+		}
+
+	/**
+	 * Threadsafe
+	 */
+	private static class SpatialHashMap
+		{
+		private static final int BIN_SIZE = 128;
+		private HashMap<Point,Set<VisualItem>> binMap = new HashMap<Point,Set<VisualItem>>();
+
+		/**
+		 * @return true if the item was added to the map,
+		 *         false if it has no bounds or empty bounds, or if the item was already mapped
+		 */
+		synchronized boolean put(VisualItem item)
+			{
+			if (item.bounds == null || item.bounds.isEmpty()) return false;
+			for (Point key : overlappingBins(item.bounds))
+				{
+				Set<VisualItem> bin = binMap.get(key);
+				if (bin == null)
+					{
+					bin = new HashSet<VisualItem>();
+					binMap.put(key,bin);
+					}
+				if (!bin.add(item))
+					return false;
+				}
+			return true;
+			}
+
+		/**
+		 * @return true if item was removed, false if it was not mapped.
+		 */
+		synchronized boolean remove(VisualItem item)
+			{
+			if (item.bounds == null || item.bounds.isEmpty()) return false;
+			for (Point key : overlappingBins(item.bounds))
+				{
+				Set<VisualItem> bin = binMap.get(key);
+				if (bin == null || !bin.remove(item))
+					return false;
+				if (bin.isEmpty())
+					binMap.remove(key);
+				}
+			return true;
+			}
+
+		/**
+		 * @return All mapped VisualItems whose bounds intersect r.
+		 */
+		public List<VisualItem> intersect(Rectangle r)
+			{
+			Set<VisualItem> result1 = new HashSet<VisualItem>();
+			synchronized (this)
+				{
+				for (Point key : overlappingBins(r))
+					{
+					Set<VisualItem> bin = binMap.get(key);
+					if (bin != null)
+						result1.addAll(bin);
+					}
+				}
+
+			List<VisualItem> result2 = new ArrayList<VisualItem>();
+			for (Iterator<VisualItem> iter = result1.iterator(); iter.hasNext();)
+				{
+				VisualItem item = iter.next();
+				if (r.intersects(item.bounds))
+					result2.add(item);
+				}
+			return result2;
+			}
+
+		/**
+		 * @return A collection of Points (keys to binMap), corresponding to the bins overlapped by rect.
+		 */
+		private Collection<Point> overlappingBins(Rectangle rect)
+			{
+			if (rect.isEmpty()) return Collections.emptyList();
+			final int binx = calculateBinCoord(rect.x);
+			final int biny = calculateBinCoord(rect.y);
+			final int binwidth = calculateBinCoord(rect.x + rect.width - 1) - binx + 1;
+			final int binheight = calculateBinCoord(rect.y + rect.height - 1) - biny + 1;
+			return new AbstractList<Point>()
+				{
+					@Override
+					public Point get(int index)
+						{
+						if (index < 0 || index >= size())
+							throw new IndexOutOfBoundsException();
+						return new Point(binx + (index % binwidth),biny + (index / binwidth));
+						}
+
+					@Override
+					public int size()
+						{
+						return binwidth * binheight;
+						}
+				};
+			}
+
+		/**
+		 * This function calculates the bin coordinate for a pixel coordinate.
+		 * With a bin size of 100, this maps coordinates the following way:
+		 * [-200, -101] -> -2
+		 * [-100, -1] -> -1
+		 * [0, 99] -> 0
+		 * [100, 199] -> 1
+		 * and so on.
+		 */
+		private int calculateBinCoord(int x)
+			{
+			return x >= 0 ? x / BIN_SIZE : ~(~x / BIN_SIZE);
+			}
 		}
 	}
