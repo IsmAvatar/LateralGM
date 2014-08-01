6b6f4676f9682c1c79ae6f5717c1ced14581f985
egofree71 <philippedepol@gmail.com>
2014-08-01 09:18:14 +0200

    Making sure the window is visible and not minimized.

--- org/lateralgm/subframes/RoomFrame.java
+++ org/lateralgm/subframes/RoomFrame.java
@@ -1467,7 +1467,6 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 		// If the user has pressed the 'room controls' button
 		if (eventSource == roomControls)
 			{
-
 			// If the window is not open
 			if (LGM.roomControlsFrame == null)
 				{
@@ -1500,6 +1499,12 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 							}
 					});
 				LGM.roomControlsFrame.pack();
+				LGM.roomControlsFrame.setVisible(true);
+				}
+			else
+				{
+				// Make sure the window is visible and not minimized
+				LGM.roomControlsFrame.setState(LGM.roomControlsFrame.NORMAL);
 				LGM.roomControlsFrame.setVisible(true);
 				}
 
