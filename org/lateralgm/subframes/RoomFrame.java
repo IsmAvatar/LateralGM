f53f6d2db731be295495e3ccf1017ead24ad6bb7
egofree71 <philippedepol@gmail.com>
2014-12-09 10:59:35 +0100

    Apply default formatter.

--- org/lateralgm/subframes/RoomFrame.java
+++ org/lateralgm/subframes/RoomFrame.java
@@ -438,40 +438,41 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 				}
 			}
 		}
-	
+
 	public static final DataFlavor INSTANCE_FLAVOR = new DataFlavor(Instance.class,"Instance"); //$NON-NLS-1$
-	
+
 	public static class ObjectListTransferable implements Transferable
-	{
-	private static final DataFlavor[] FLAVORS = { INSTANCE_FLAVOR };
-	private final List<Instance> instanceList;
-
-	public ObjectListTransferable(List<Instance> list)
 		{
-		instanceList = list;
-		}
+		private static final DataFlavor[] FLAVORS = { INSTANCE_FLAVOR };
+		private final List<Instance> instanceList;
 
-	public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException
-		{
-		if (flavor == INSTANCE_FLAVOR)
+		public ObjectListTransferable(List<Instance> list)
 			{
-			return instanceList;
+			instanceList = list;
 			}
-		throw new UnsupportedFlavorException(flavor);
+
+		public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException
+			{
+			if (flavor == INSTANCE_FLAVOR)
+				{
+				return instanceList;
+				}
+			throw new UnsupportedFlavorException(flavor);
+			}
+
+		public DataFlavor[] getTransferDataFlavors()
+			{
+			return FLAVORS;
+			}
+
+		public boolean isDataFlavorSupported(DataFlavor flavor)
+			{
+			return flavor == INSTANCE_FLAVOR;
+			}
 		}
 
-	public DataFlavor[] getTransferDataFlavors()
+	public class ObjectListTransferHandler extends TransferHandler
 		{
-		return FLAVORS;
-		}
-
-	public boolean isDataFlavorSupported(DataFlavor flavor)
-		{
-		return flavor == INSTANCE_FLAVOR;
-		}
-	}
-	
-	public class ObjectListTransferHandler extends TransferHandler {
 		/**
 		 * TODO: Change if needed.
 		 */
@@ -479,78 +480,91 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 		private int[] indices = null;
 		private ArrayList<Instance> instanceList = null;
 		private int addIndex = -1; //Location where items were added
-		private int addCount = 0;  //Number of items added.
-		      
+		private int addCount = 0; //Number of items added.
+
 		public ObjectListTransferHandler(ActiveArrayList<Instance> insts)
 			{
 			instanceList = insts;
 			}
 
-		public boolean canImport(TransferHandler.TransferSupport info) {
-		    return info.isDataFlavorSupported(INSTANCE_FLAVOR);
-		}
-	
-	  protected Transferable createTransferable(JComponent c) {
-	      JList<Instance> list = (JList<Instance>)c;
-	      indices  = list.getSelectedIndices();
-	      return new ObjectListTransferable(list.getSelectedValuesList());
-	  }
-	  
-	  /**
-	   * We support both copy and move actions.
-	   */
-	  public int getSourceActions(JComponent c) {
-	      return TransferHandler.COPY_OR_MOVE;
-	  }
-	  
-	  /**
-	   * Perform the actual import.  This demo only supports drag and drop.
-	   */
-	  public boolean importData(TransferHandler.TransferSupport info) {
-	      if (!info.isDrop()) {
-	          return false;
-	      }
-	      
-	      JList.DropLocation dl = (JList.DropLocation)info.getDropLocation();
-	      int index = dl.getIndex();
-	      boolean insert = dl.isInsert();
+		public boolean canImport(TransferHandler.TransferSupport info)
+			{
+			return info.isDataFlavorSupported(INSTANCE_FLAVOR);
+			}
 
-	      // Get the instance that is being dropped.
-	      Transferable t = info.getTransferable();
-	      List<Instance> data = null;
-	      
-	      try {
-	          data = (List<Instance>) t.getTransferData(INSTANCE_FLAVOR);
-	      } 
-	      catch (Exception e) {  LGM.showDefaultExceptionHandler(e); }
-	      
-	      addIndex = index;
-	      addCount = data.size();
-	      
-	      // Perform the actual import.  
-	      for (int i = 0; i < addCount; i++) {
-	      	instanceList.add(index++, data.get(i));
-	      }
-	      return true;
-	  }
-	
-	  /**
-	   * Remove the items moved from the list.
-	   */
-	  protected void exportDone(JComponent c, Transferable data, int action) {
-	      JList<Instance> source = (JList<Instance>)c;
-	      
-	      if (action == TransferHandler.MOVE) {
-	          for (int i = indices.length - 1; i >= 0; i--) {
-	              instanceList.remove(indices[i]);
-	          }
-	      }
-	      
-	      indices = null;
-	      addCount = 0;
-	      addIndex = -1;
-	  }
-	}
+		protected Transferable createTransferable(JComponent c)
+			{
+			JList<Instance> list = (JList<Instance>) c;
+			indices = list.getSelectedIndices();
+			return new ObjectListTransferable(list.getSelectedValuesList());
+			}
+
+		/**
+		 * We support both copy and move actions.
+		 */
+		public int getSourceActions(JComponent c)
+			{
+			return TransferHandler.COPY_OR_MOVE;
+			}
+
+		/**
+		 * Perform the actual import.  This demo only supports drag and drop.
+		 */
+		public boolean importData(TransferHandler.TransferSupport info)
+			{
+			if (!info.isDrop())
+				{
+				return false;
+				}
+
+			JList.DropLocation dl = (JList.DropLocation) info.getDropLocation();
+			int index = dl.getIndex();
+			boolean insert = dl.isInsert();
+
+			// Get the instance that is being dropped.
+			Transferable t = info.getTransferable();
+			List<Instance> data = null;
+
+			try
+				{
+				data = (List<Instance>) t.getTransferData(INSTANCE_FLAVOR);
+				}
+			catch (Exception e)
+				{
+				LGM.showDefaultExceptionHandler(e);
+				}
+
+			addIndex = index;
+			addCount = data.size();
+
+			// Perform the actual import.  
+			for (int i = 0; i < addCount; i++)
+				{
+				instanceList.add(index++,data.get(i));
+				}
+			return true;
+			}
+
+		/**
+		 * Remove the items moved from the list.
+		 */
+		protected void exportDone(JComponent c, Transferable data, int action)
+			{
+			JList<Instance> source = (JList<Instance>) c;
+
+			if (action == TransferHandler.MOVE)
+				{
+				for (int i = indices.length - 1; i >= 0; i--)
+					{
+					instanceList.remove(indices[i]);
+					}
+				}
+
+			indices = null;
+			addCount = 0;
+			addIndex = -1;
+			}
+		}
 
 	public JPanel makeObjectsPane()
 		{
@@ -909,14 +923,13 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 		panel.setLayout(layout);
 
 		JLabel layer = new JLabel(Messages.getString("RoomFrame.CURRENT_TILE_LAYER"));
-		
+
 		Room currentRoom = editor.getRoom();
 		int firstTileLayer = 0;
-		
+
 		// If there is already tiles in the room, use the first tile layer as the current layer
-		if (!currentRoom.tiles.isEmpty())
-			firstTileLayer = currentRoom.tiles.get(0).getDepth();
-		
+		if (!currentRoom.tiles.isEmpty()) firstTileLayer = currentRoom.tiles.get(0).getDepth();
+
 		taDepth = new NumberField(Integer.MIN_VALUE,Integer.MAX_VALUE,firstTileLayer);
 		taDepth.setMaximumSize(new Dimension(Integer.MAX_VALUE,taDepth.getHeight()));
 
@@ -1747,7 +1760,7 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 
 	// Window which displays the room controls
 	public static JFrame roomControlsFrame;
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 		{
@@ -2189,13 +2202,13 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 
 		// Get the image dimension
 		ResourceReference<GmObject> instanceObject = instance.properties.get(PInstance.OBJECT);
-		
+
 		BufferedImage instanceImage = null;
-		if (instanceObject != null) {
+		if (instanceObject != null)
+			{
 			GmObject inst = instanceObject.get();
-			if (inst != null)
-				instanceImage = inst.getDisplayImage();
-		}
+			if (inst != null) instanceImage = inst.getDisplayImage();
+			}
 
 		if (instanceImage == null)
 			{
