bb32b863aa702938677ac7fb93d171d036c725d0
egofree71 <philippedepol@gmail.com>
2014-12-14 11:29:02 +0100

    clean-up.

--- org/lateralgm/components/visual/RoomEditor.java
+++ org/lateralgm/components/visual/RoomEditor.java
@@ -20,7 +20,6 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.EnumSet;
