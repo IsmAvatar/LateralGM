c4023c6accf3848ccaf1db25deb6fe180e33dafd
egofree71 <philippedepol@gmail.com>
2014-12-29 20:21:49 +0100

    Fixing typo.

--- org/lateralgm/components/CodeTextArea.java
+++ org/lateralgm/components/CodeTextArea.java
@@ -628,6 +628,7 @@ public class CodeTextArea extends JoshTextPanel implements UpdateListener,Action
 			catch (PrinterException e)
 				{
 				LGM.showDefaultExceptionHandler(e);
+				}
 			}
 		else if (com.equals("JoshText.UNDO"))
 			{
