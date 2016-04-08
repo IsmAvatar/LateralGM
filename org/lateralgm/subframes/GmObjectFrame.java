e25d2f12a50eb3703508cb7a450fa48b603917c2
RobertBColton <robertbcolton@gmail.com>
2016-04-08 00:15:45 -0400

    Add javadoc comment for helper method.

--- org/lateralgm/subframes/GmObjectFrame.java
+++ org/lateralgm/subframes/GmObjectFrame.java
@@ -695,6 +695,17 @@ public class GmObjectFrame extends InstantiableResourceFrame<GmObject,PGmObject>
 		super.actionPerformed(e);
 		}
 
+	/**
+	 * Check if the node is non-null, is an instance of EventInstanceNode and that it has a parent
+	 * and still exists in the events tree. This is useful with 
+	 * {@link javax.swing.JTree#getLastSelectedPathComponent() getLastSelectedPathComponent()} or
+	 * {@link javax.swing.tree.TreePath#getLastPathComponent() getLastPathComponent()} because they
+	 * can return nodes already removed from the tree.
+	 * 
+	 * @param node The node to check for validity.
+	 * 
+	 * @return Whether the node is valid.
+	 */
 	private static boolean isValidEventInstanceNode(Object node)
 		{
 		return (node != null && node instanceof EventInstanceNode
