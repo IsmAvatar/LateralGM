74ebd27c87e39b0768f5ca9e32a13b0e921d9f94
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2017-12-31 23:19:21 -0500

    switch to semantic versioning

--- org/lateralgm/main/LGM.java
+++ org/lateralgm/main/LGM.java
@@ -180,7 +180,7 @@ import org.lateralgm.subframes.TimelineFrame;
 
 public final class LGM
 	{
-	public static final String version = "1.8.7.18"; //$NON-NLS-1$
+	public static final String version = "1.8.8"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.
