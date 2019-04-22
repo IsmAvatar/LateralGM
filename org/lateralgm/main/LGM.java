a4518c3b2026c4ca5dd44ca6a72ac5406c6027b9
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-04-22 10:24:51 -0400

    increment release version to 1.8.52

--- org/lateralgm/main/LGM.java
+++ org/lateralgm/main/LGM.java
@@ -134,7 +134,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.51"; //$NON-NLS-1$
+	public static final String version = "1.8.52"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.
