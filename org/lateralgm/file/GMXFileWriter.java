01710bed1fc68c35d672cd1cdcb322481416b5cb
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-05-22 02:08:32 -0400

    add GMX include writing

--- org/lateralgm/file/GMXFileWriter.java
+++ org/lateralgm/file/GMXFileWriter.java
@@ -71,6 +71,7 @@ import org.lateralgm.resources.GameSettings.PGameSettings;
 import org.lateralgm.resources.GmObject;
 import org.lateralgm.resources.GmObject.PGmObject;
 import org.lateralgm.resources.Include;
+import org.lateralgm.resources.Include.PInclude;
 import org.lateralgm.resources.InstantiableResource;
 import org.lateralgm.resources.Path;
 import org.lateralgm.resources.Path.PPath;
@@ -236,7 +237,7 @@ public final class GMXFileWriter
 		LGM.setProgress(100,Messages.getString("ProgressDialog.ROOMS")); //$NON-NLS-1$
 		writeGroup(c,root,Room.class);
 		LGM.setProgress(110,Messages.getString("ProgressDialog.INCLUDEFILES")); //$NON-NLS-1$
-		//writeGroup(c,root,Include.class);
+		writeGroup(c,root,Include.class);
 		LGM.setProgress(120,Messages.getString("ProgressDialog.PACKAGES")); //$NON-NLS-1$
 		//writePackages(c, root);
 		LGM.setProgress(130,Messages.getString("ProgressDialog.CONSTANTS")); //$NON-NLS-1$
@@ -327,7 +328,7 @@ public final class GMXFileWriter
 		}
 
 	// This is used to store booleans since GMX uses -1 and 0 and other times false and true
-	private static String boolToString(boolean bool)
+	private static String boolToString(Boolean bool)
 		{
 			return bool ? "-1" : "0"; //$NON-NLS-1$ //$NON-NLS-2$
 		}
@@ -417,6 +418,8 @@ public final class GMXFileWriter
 						writeGmObject(c,resNode,domRoot);
 					else if (kind == Room.class)
 						writeRoom(c,resNode,domRoot);
+					else if (kind == Include.class)
+						writeInclude(c,resNode,domRoot);
 					break;
 				}
 			}
@@ -1279,9 +1282,36 @@ public final class GMXFileWriter
 		domRoot.appendChild(res);
 		}
 
-	public static void writeIncludedFiles(ProjectFileContext c, Element root) throws IOException
+	private static void writeInclude(ProjectFileContext c, ResNode resNode, Element domRoot)
+			throws IOException
 		{
-		// TODO: Implement
+		ProjectFile f = c.f;
+		Document dom = c.dom;
+
+		Include include = (Include) resNode.getRes().get();
+		Element incRoot = dom.createElement("datafile"); //$NON-NLS-1$
+		incRoot.appendChild(createElement(dom,"name", include.getName())); //$NON-NLS-1$
+		incRoot.appendChild(createElement(dom,"filename", //$NON-NLS-1$
+				include.get(PInclude.FILENAME).toString()));
+		incRoot.appendChild(createElement(dom,"size", //$NON-NLS-1$
+				include.get(PInclude.SIZE).toString()));
+		incRoot.appendChild(createElement(dom,"exportDir", //$NON-NLS-1$
+				include.get(PInclude.EXPORTFOLDER).toString()));
+		incRoot.appendChild(createElement(dom,"exportAction", //$NON-NLS-1$
+				include.get(PInclude.EXPORTACTION).toString()));
+		incRoot.appendChild(createElement(dom,"overwrite", //$NON-NLS-1$
+				boolToString((Boolean)include.get(PInclude.OVERWRITE))));
+		incRoot.appendChild(createElement(dom,"store", //$NON-NLS-1$
+				boolToString((Boolean)include.get(PInclude.STORE))));
+		incRoot.appendChild(createElement(dom,"freeData", //$NON-NLS-1$
+				boolToString((Boolean)include.get(PInclude.FREEMEMORY))));
+		incRoot.appendChild(createElement(dom,"removeEnd", //$NON-NLS-1$
+				boolToString((Boolean)include.get(PInclude.REMOVEATGAMEEND))));
+		domRoot.appendChild(incRoot);
+
+		String folderName = f.getDirectory() + "/datafiles"; //$NON-NLS-1$
+		File folderFile = new File(Util.getPOSIXPath(folderName));
+		folderFile.mkdir();
 		}
 
 	public static void writePackages(ProjectFileContext c, Element root) throws IOException
