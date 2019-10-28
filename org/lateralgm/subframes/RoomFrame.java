83b9931a090d8ff80004afce18abab3b142b0fe9
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-10-28 15:53:45 -0400

    accept negative x/y instance scale in room editor

--- org/lateralgm/subframes/RoomFrame.java
+++ org/lateralgm/subframes/RoomFrame.java
@@ -910,10 +910,10 @@ public class RoomFrame extends InstantiableResourceFrame<Room,PRoom> implements
 		objectVerticalPosition.setColumns(4);
 		objectVerticalPosition.addFocusListener(this);
 		JLabel lObjScaleX = new JLabel(Messages.getString("RoomFrame.SCALE_X")); //$NON-NLS-1$
-		objectScaleX = new NumberField(0.1,9999.0,1.0,2);
+		objectScaleX = new NumberField(-9999.0,9999.0,1.0,2);
 		objectScaleX.addFocusListener(this);
 		JLabel lObjScaleY = new JLabel(Messages.getString("RoomFrame.SCALE_Y")); //$NON-NLS-1$
-		objectScaleY = new NumberField(0.1,9999.0,1.0,2);
+		objectScaleY = new NumberField(-9999.0,9999.0,1.0,2);
 		objectScaleY.addFocusListener(this);
 		JLabel lObjRotation = new JLabel(Messages.getString("RoomFrame.ROTATION")); //$NON-NLS-1$
 		objectRotation = new NumberField(0.0,360.0,0.0,2);
