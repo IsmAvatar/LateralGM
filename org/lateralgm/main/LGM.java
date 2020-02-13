IsmAvatar/LateralGM

commit 071b3eafe68e0d95d0e36b08299d5ef73168e20a

Implements AutoComBox class which is a JComboBox interface with auto-completion capabilities. It is currently used in the font editor at the behest of TheExDeus and myself.

org/lateralgm/subframes/FontFrame.java
@@ -65,6 +65,7 @@ import org.lateralgm.ui.swing.propertylink.FormattedLink;
 import org.lateralgm.ui.swing.propertylink.PropertyLinkFactory;
 import org.lateralgm.ui.swing.propertylink.ComboBoxLink.IndexComboBoxConversion;
 import org.lateralgm.ui.swing.util.ArrayListModel;
+import org.lateralgm.util.AutoComboBox;
 import org.lateralgm.util.PropertyLink;
 import org.lateralgm.util.PropertyMap.PropertyUpdateEvent;
 import org.lateralgm.util.PropertyMap.PropertyUpdateListener;
@@ -119,9 +120,9 @@ public class FontFrame extends InstantiableResourceFrame<Font,PFont> implements 
 		JLabel lName = new JLabel(Messages.getString("FontFrame.NAME")); //$NON-NLS-1$
 
 		JLabel lFont = new JLabel(Messages.getString("FontFrame.FONT")); //$NON-NLS-1$
-		fonts = new JComboBox<String>(
+		fonts = new AutoComboBox<String>(
 				GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
-		fonts.setEditable(true);
+		//fonts.setEditable(true);
 		plf.make(fonts,PFont.FONT_NAME,null);
 
 		JLabel lSize = new JLabel(Messages.getString("FontFrame.SIZE")); //$NON-NLS-1$

org/lateralgm/util/AutoComboBox.java
@@ -1,0 +1,144 @@
+/**
+* @file  AutoComboBox.java
+* @brief Class implementing a JComboBox class with auto-completion features.
+*
+* @section License
+*
+* Copyright (C) 2014 Robert B. Colton
+* This file is a part of the LateralGM IDE.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+package org.lateralgm.util;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.text.*;
+
+public class AutoComboBox<T> extends JComboBox<T> {
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = -2358279864039989215L;
+	
+		JTextComponent editor;
+		DocumentFilter document;
+    // flag to indicate if setSelectedItem has been called
+    // subsequent calls to remove/insertString should be ignored
+    boolean selecting=false;
+    
+    public AutoComboBox(T[] items) {
+    		super(items);
+    		this.setEditable(true);
+        editor = (JTextComponent) getEditor().getEditorComponent();
+        // change the editor's document
+        document = new DocumentFilter();
+        editor.setDocument(document);
+        
+        addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!selecting) document.highlightCompletedText(0);
+            }
+        });
+        editor.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (isDisplayable()) setPopupVisible(true);
+            }
+        });
+    }
+    
+    private Object lookupItem(String pattern) {
+        Object selectedItem = getSelectedItem();
+        Object ret = null;
+        // only search for a different item if the currently selected does not match
+        if (selectedItem != null) {
+        	if (selectedItem.toString().startsWith(pattern)) {
+        		return selectedItem;
+        	} else if (selectedItem.toString().toUpperCase().startsWith(pattern.toUpperCase())) {
+        		ret = selectedItem;
+        	}
+        }
+        
+        // iterate over all items
+    		ComboBoxModel<T> model = this.getModel();
+    		
+        for (int i = 0, n = model.getSize(); i < n; i++) {
+            Object currentItem = model.getElementAt(i);
+            // current item starts with the pattern?
+            if (currentItem.toString().startsWith(pattern)) {
+              return currentItem;
+            } else if (currentItem.toString().toUpperCase().startsWith(pattern.toUpperCase())) {
+            	ret = currentItem;
+            }
+        }
+
+        // no item starts with the pattern => return null
+        return ret;
+    }
+    
+    public void setSelectedItem(Object item) {
+	    selecting = true;
+	    super.setSelectedItem(item);
+	    selecting = false;
+    }
+    
+    public class DocumentFilter extends PlainDocument {
+    
+    /**
+			 * 
+			 */
+			private static final long serialVersionUID = 7860466445717335935L;
+
+		public void remove(int offs, int len) throws BadLocationException {
+        // return immediately when selecting an item
+        //if (selecting) return;
+        super.remove(offs, len);
+    }
+    
+    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+        // insert the string into the document
+        super.insertString(offs, str, a);
+        // return immediately when selecting an item
+        if (selecting) return;
+        // lookup and select a matching item
+        Object item = lookupItem(getText(0, getLength()));
+        if (item != null) {
+            setSelectedItem(item);
+        } else {
+            // keep old item selected if there is no match
+            item = getSelectedItem();
+            // imitate no insert (later on offs will be incremented by str.length(): selection won't move forward)
+            offs = offs-str.length();
+            // provide feedback to the user that his input has been received but can not be accepted
+            getToolkit().beep(); // when available use: UIManager.getLookAndFeel().provideErrorFeedback(comboBox);
+        }
+        setText(item.toString());
+        // select the completed part
+        highlightCompletedText(offs+str.length());
+    }
+    
+    private void setText(String text) throws BadLocationException {
+        // remove all text and insert the completed string
+        super.remove(0, getLength());
+        super.insertString(0, text, null);
+    }
+    
+    private void highlightCompletedText(int start) {
+        editor.setSelectionStart(start);
+        editor.setSelectionEnd(getLength());
+    }
+  
+  }
+}

org/lateralgm/subframes/ResourceInfoFrame.java
@@ -1,22 +1,25 @@
-/*
- * Copyright (C) 2013 Robert B. Colton
- * 
- * This file is part of LateralGM.
- * 
- * LateralGM is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * LateralGM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License (COPYING) for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
+/**
+* @file  ResourceInfoFrame.java
+* @brief Class implementing a resource information frame.
+*
+* @section License
+*
+* Copyright (C) 2013-2014 Robert B. Colton
+* This file is a part of the LateralGM IDE.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
 package org.lateralgm.subframes;
 
 import java.awt.BorderLayout;

commit beb1eb0ba25c509b3b773dbdfffa05d52a18d1c4

Fix Autocomplete ComboBox Corrects a minor oversight in 071b3eafe68e0d95d0e36b08299d5ef73168e20a which caused the autocomplete combo to have no text by default. Simply changing it to set the combo editable after the filter document is applied seems to correct it and the font frame now has the correct name of the selected font when opened. This was only reproducible with the default "Arial" font.

org/lateralgm/main/LGM.java
@@ -135,7 +135,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.91"; //$NON-NLS-1$
+	public static final String version = "1.8.92"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.

org/lateralgm/subframes/FontFrame.java
@@ -129,7 +129,6 @@ public class FontFrame extends InstantiableResourceFrame<Font,PFont> implements
 		JLabel lFont = new JLabel(Messages.getString("FontFrame.FONT")); //$NON-NLS-1$
 		fonts = new AutoComboBox<String>(
 				GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
-		//fonts.setEditable(true);
 		plf.make(fonts,PFont.FONT_NAME,null);
 
 		JLabel lSize = new JLabel(Messages.getString("FontFrame.SIZE")); //$NON-NLS-1$

org/lateralgm/util/AutoComboBox.java
@@ -43,11 +43,11 @@ public class AutoComboBox<T> extends JComboBox<T>
 	public AutoComboBox(T[] items)
 		{
 		super(items);
-		this.setEditable(true);
 		editor = (JTextComponent) getEditor().getEditorComponent();
 		// change the editor's document
 		document = new DocumentFilter();
 		editor.setDocument(document);
+		this.setEditable(true);
 
 		addActionListener(new ActionListener()
 			{