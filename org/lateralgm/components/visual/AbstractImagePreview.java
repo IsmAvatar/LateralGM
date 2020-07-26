IsmAvatar/LateralGM

commit c4a130db77fac82932283400aa44b055af35902d

Fix Image Preview Jittering Regression Setting the size in 91be58d31c59ca8d656b6f1deba0bdc67eef1730 was inconsistent with how we've always used the preferred size. The simple answer was to instead revalidate when the preffered size changes, such as by zooming, which causes the component to be layed out again using its preferred size. This actually fixes an issue in the sprite frame playback where it was jittery and you could not drag the sprite origin with the mouse while animation was playing. That's because the setSize call was causing the component to end up in the top-left of the scroll viewport instead of the center. I also abstracted the resizeAndRepaint logic since it's the same JTable uses internally and put it into the abstract image preview parent class so it doesn't keep being duplicated.

org/lateralgm/components/visual/AbstractImagePreview.java
@@ -47,6 +47,21 @@ public abstract class AbstractImagePreview extends JLabel
 		return new Dimension(bi.getWidth(),bi.getHeight());
 		}
 
+	/**
+	 * Refreshes the image preview by laying it out to its
+	 * preferred dimensions and then repainting it. Follows the
+	 * same conventions that {@code JTable.resizeAndRepaint} and
+	 * other Swing components do internally. It is not to be
+	 * used when the size hasn't actually changed, e.g
+	 * transparency pixel removed. In that case, only a simple
+	 * repaint is all that is needed.
+	 */
+	protected void resizeAndRepaint()
+		{
+		revalidate(); // size change, do layout
+		repaint(); // repaint it with new size
+		}
+
 	@Deprecated
 	public void setIcon(Icon ico)
 		{

org/lateralgm/main/LGM.java
@@ -133,7 +133,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.167"; //$NON-NLS-1$
+	public static final String version = "1.8.168"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.

org/lateralgm/components/visual/BackgroundPreview.java
@@ -47,10 +47,6 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 		b.reference.updateSource.addListener(this);
 		}
 
-	@Override public void setSize(Dimension d) {
-		super.setSize(d);
-	}
-
 	protected BufferedImage getTransparentImage()
 		{
 		if (background == null) return null;
@@ -163,10 +159,7 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	protected BufferedImage getImage()
@@ -177,10 +170,7 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 	public void updated(UpdateEvent e)
 		{
 		// new image may be new size
-		// therefore, do layout again
-		revalidate();
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	private class BackgroundPropertyListener extends PropertyUpdateListener<PBackground>

org/lateralgm/components/visual/SubimagePreview.java
@@ -181,19 +181,14 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	public void setIndex(int i)
 		{
 		subIndex = i;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		// we allow varying subimage size
+		this.resizeAndRepaint();
 		}
 
 	public int getIndex()
@@ -203,14 +198,14 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 
 	public void setShowBbox(boolean show)
 		{
-		if (show != showBbox) repaint();
-		showBbox = show;
+		if (show != showBbox) repaint(); // << posts event
+		showBbox = show; // << repaint event happens later
 		}
 
 	public void setShowOrigin(boolean show)
 		{
-		if (show != showOrigin) repaint();
-		showOrigin = show;
+		if (show != showOrigin) repaint(); // << posts event
+		showOrigin = show; // << repaint event happens later
 		}
 
 	protected void processMouseEvent(MouseEvent e)
@@ -263,10 +258,7 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 	public void updated(UpdateEvent e)
 		{
 		// new image may be new size
-		// therefore, do layout again
-		revalidate();
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	private class SpritePropertyListener extends PropertyUpdateListener<PSprite>

commit 0b64887d7940de26bdff8282b039a443cd4230a1

Cleanup Zoom In/Out Logic Move it to the abstract image preview parent class and fix a regression from c4a130db77fac82932283400aa44b055af35902d by validating the scroll.

org/lateralgm/components/visual/AbstractImagePreview.java
@@ -11,12 +11,14 @@ package org.lateralgm.components.visual;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.image.BufferedImage;
 
 import javax.swing.Icon;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public abstract class AbstractImagePreview extends JLabel
 	{
@@ -58,6 +60,44 @@ public abstract class AbstractImagePreview extends JLabel
 		{
 		zoom = nzoom;
 		this.resizeAndRepaint();
+		}
+
+	/** Zoom in, centering around a specific point, usually the mouse. */
+	public void zoomIn(Point point, JScrollPane scroll)
+		{
+		if (this.getZoom() >= 32) return;
+		this.setZoom(this.getZoom() * 2);
+		scroll.validate();
+		Dimension size = scroll.getViewport().getSize();
+
+		int newX = (int) (point.x * 2) - size.width / 2;
+		int newY = (int) (point.y * 2) - size.height / 2;
+		setLocation(new Point(-newX,-newY));
+		}
+
+	/** Zoom out, centering around a specific point, usually the mouse. */
+	public void zoomOut(Point point, JScrollPane scroll)
+		{
+		if (this.getZoom() <= 0.04) return;
+		this.setZoom(this.getZoom() / 2);
+		scroll.validate();
+		Dimension size = scroll.getViewport().getSize();
+
+		int newX = (int) (point.x / 2) - size.width / 2;
+		int newY = (int) (point.y / 2) - size.height / 2;
+		setLocation(new Point(-newX,-newY));
+		}
+
+	public void zoomIn(JScrollPane scroll)
+		{
+		Dimension size = this.getPreferredSize();
+		zoomIn(new Point(size.width/2,size.height/2),scroll);
+		}
+
+	public void zoomOut(JScrollPane scroll)
+		{
+		Dimension size = this.getPreferredSize();
+		zoomOut(new Point(size.width/2,size.height/2),scroll);
 		}
 
 	/**

org/lateralgm/main/LGM.java
@@ -133,7 +133,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.170"; //$NON-NLS-1$
+	public static final String version = "1.8.171"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.

org/lateralgm/subframes/BackgroundFrame.java
@@ -18,7 +18,6 @@ import java.awt.BorderLayout;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -99,48 +98,6 @@ public class BackgroundFrame extends InstantiableResourceFrame<Background,PBackg
 
 	private final BackgroundPropertyListener bpl = new BackgroundPropertyListener();
 
-	/** Zoom in, centering around a specific point, usually the mouse. */
-	public void zoomIn(Point point)
-		{
-		if (this.getZoom() >= 32) return;
-		this.setZoom(this.getZoom() * 2);
-		Dimension size = previewScroll.getViewport().getSize();
-
-		int newX = (int) (point.x * 2) - size.width / 2;
-		int newY = (int) (point.y * 2) - size.height / 2;
-		previewScroll.getViewport().setViewPosition(new Point(newX,newY));
-
-		previewScroll.revalidate();
-		previewScroll.repaint();
-		}
-
-	/** Zoom out, centering around a specific point, usually the mouse. */
-	public void zoomOut(Point point)
-		{
-		if (this.getZoom() <= 0.04) return;
-		this.setZoom(this.getZoom() / 2);
-		Dimension size = previewScroll.getViewport().getSize();
-
-		int newX = (int) (point.x / 2) - size.width / 2;
-		int newY = (int) (point.y / 2) - size.height / 2;
-		previewScroll.getViewport().setViewPosition(new Point(newX,newY));
-
-		previewScroll.revalidate();
-		previewScroll.repaint();
-		}
-
-	public void zoomIn()
-		{
-		Dimension size = previewScroll.getViewport().getViewSize();
-		zoomIn(new Point(size.width/2,size.height/2));
-		}
-
-	public void zoomOut()
-		{
-		Dimension size = previewScroll.getViewport().getViewSize();
-		zoomOut(new Point(size.width/2,size.height/2));
-		}
-
 	public BackgroundFrame(Background res, ResNode node)
 		{
 		super(res,node);
@@ -174,11 +131,11 @@ public class BackgroundFrame extends InstantiableResourceFrame<Background,PBackg
 					{
 					if (ev.getButton() == MouseEvent.BUTTON1)
 						{
-						zoomIn(ev.getPoint());
+						preview.zoomIn(ev.getPoint(),previewScroll);
 						}
 					if (ev.getButton() == MouseEvent.BUTTON3)
 						{
-						zoomOut(ev.getPoint());
+						preview.zoomOut(ev.getPoint(),previewScroll);
 						}
 					preview.setCursor(LGM.zoomCursor);
 					}
@@ -526,12 +483,12 @@ public class BackgroundFrame extends InstantiableResourceFrame<Background,PBackg
 			}
 		else if (cmd.endsWith(".ZOOM_IN")) //$NON-NLS-1$
 			{
-			zoomIn();
+			preview.zoomIn(previewScroll);
 			return;
 			}
 		else if (cmd.endsWith(".ZOOM_OUT")) //$NON-NLS-1$
 			{
-			zoomOut();
+			preview.zoomOut(previewScroll);
 			return;
 			}
 		}

org/lateralgm/subframes/SpriteFrame.java
@@ -24,7 +24,6 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
@@ -168,48 +167,6 @@ public class SpriteFrame extends InstantiableResourceFrame<Sprite,PSprite> imple
 	private Map<BufferedImage,ImageEditor> editors;
 	private MouseAdapter previewMouseAdapter;
 
-	/** Zoom in, centering around a specific point, usually the mouse. */
-	public void zoomIn(Point point)
-		{
-		if (this.getZoom() >= 32) return;
-		this.setZoom(this.getZoom() * 2);
-		Dimension size = previewScroll.getViewport().getSize();
-
-		int newX = (int) (point.x * 2) - size.width / 2;
-		int newY = (int) (point.y * 2) - size.height / 2;
-		previewScroll.getViewport().setViewPosition(new Point(newX,newY));
-
-		previewScroll.revalidate();
-		previewScroll.repaint();
-		}
-
-	/** Zoom out, centering around a specific point, usually the mouse. */
-	public void zoomOut(Point point)
-		{
-		if (this.getZoom() <= 0.04) return;
-		this.setZoom(this.getZoom() / 2);
-		Dimension size = previewScroll.getViewport().getSize();
-
-		int newX = (int) (point.x / 2) - size.width / 2;
-		int newY = (int) (point.y / 2) - size.height / 2;
-		previewScroll.getViewport().setViewPosition(new Point(newX,newY));
-
-		previewScroll.revalidate();
-		previewScroll.repaint();
-		}
-
-	public void zoomIn()
-		{
-		Dimension size = previewScroll.getViewport().getViewSize();
-		zoomIn(new Point(size.width/2,size.height/2));
-		}
-
-	public void zoomOut()
-		{
-		Dimension size = previewScroll.getViewport().getViewSize();
-		zoomOut(new Point(size.width/2,size.height/2));
-		}
-
 	public SpriteFrame(Sprite res, ResNode node)
 		{
 		super(res,node);
@@ -248,11 +205,11 @@ public class SpriteFrame extends InstantiableResourceFrame<Sprite,PSprite> imple
 					{
 					if (ev.getButton() == MouseEvent.BUTTON1)
 						{
-						zoomIn(ev.getPoint());
+						preview.zoomIn(ev.getPoint(),previewScroll);
 						}
 					if (ev.getButton() == MouseEvent.BUTTON3)
 						{
-						zoomOut(ev.getPoint());
+						preview.zoomOut(ev.getPoint(),previewScroll);
 						}
 					preview.setCursor(LGM.zoomCursor);
 					}
@@ -1400,12 +1357,12 @@ public class SpriteFrame extends InstantiableResourceFrame<Sprite,PSprite> imple
 			}
 		else if (e.getSource() == zoomIn)
 			{
-			zoomIn();
+			preview.zoomIn(previewScroll);
 			return;
 			}
 		else if (e.getSource() == zoomOut)
 			{
-			zoomOut();
+			preview.zoomOut(previewScroll);
 			return;
 			}
 		else if (e.getSource() == play)