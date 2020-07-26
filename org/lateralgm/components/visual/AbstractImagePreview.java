IsmAvatar/LateralGM

commit 91be58d31c59ca8d656b6f1deba0bdc67eef1730

Cleanup & Optimize Image Preview Repaints Fixes a widespread mistake from aefd4427229a1003059acd7eeabd3bc5cb5f7519 & 497c7c767201648b3ce2c0e0e7ea0b0e4cf702c9 and others. The updateUI method is not appropriate for simple repainting, it's slow and reinstalls the entire look and feel. It's only for when the look and feel is changing and is already being handled correctly. A revalidation is all that is necessary when the component size changes and a repaint is all that is needed when the model changes. Also removes a graphics dispose from SubimagePreview that I mistakenly added in e35a1acc6ba36b19a3b48bcf4a5f4e61c59b8966 that should not have been there according to the documentation it is only needed after creating custom graphics or cloning the provided one.

org/lateralgm/components/visual/BackgroundPreview.java
@@ -163,8 +163,10 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
+		// size change automatically revalidates
 		this.setSize(this.getPreferredSize());
-		updateUI();
+		// paint it again too
+		repaint();
 		}
 
 	protected BufferedImage getImage()
@@ -174,7 +176,11 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 
 	public void updated(UpdateEvent e)
 		{
-		updateUI();
+		// new image may be new size
+		// therefore, do layout again
+		revalidate();
+		// paint it again too
+		repaint();
 		}
 
 	private class BackgroundPropertyListener extends PropertyUpdateListener<PBackground>
@@ -185,7 +191,7 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 				{
 				case PRELOAD:
 				case SMOOTH_EDGES:
-				case TRANSPARENT:
+				case TRANSPARENT: // << handled by UpdateEvent ^^
 					return;
 				default:
 					repaint();

org/lateralgm/components/visual/SubimagePreview.java
@@ -156,7 +156,6 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 			}
 
 		g.setClip(oldClip); //restore the clip
-		g.dispose();
 		}
 
 	private void setBoundedOrigin(int x, int y)
@@ -182,15 +181,19 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
+		// size change automatically revalidates
 		this.setSize(this.getPreferredSize());
-		updateUI();
+		// paint it again too
+		repaint();
 		}
 
 	public void setIndex(int i)
 		{
 		subIndex = i;
-		updateUI();
-		this.repaint();
+		// size change automatically revalidates
+		this.setSize(this.getPreferredSize());
+		// paint it again too
+		repaint();
 		}
 
 	public int getIndex()
@@ -257,7 +260,11 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 
 	public void updated(UpdateEvent e)
 		{
-		updateUI();
+		// new image may be new size
+		// therefore, do layout again
+		revalidate();
+		// paint it again too
+		repaint();
 		}
 
 	private class SpritePropertyListener extends PropertyUpdateListener<PSprite>
@@ -268,7 +275,7 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 				{
 				case PRELOAD:
 				case SMOOTH_EDGES:
-				case TRANSPARENT:
+				case TRANSPARENT: // << handled by UpdateEvent ^^
 				case SHAPE:
 					return;
 				default:

org/lateralgm/main/LGM.java
@@ -133,7 +133,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.164"; //$NON-NLS-1$
+	public static final String version = "1.8.165"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.

org/lateralgm/components/visual/SpriteStripPreview.java
@@ -87,6 +87,6 @@ public class SpriteStripPreview extends AbstractImagePreview implements ValueCha
 
 	public void valueChange(ValueChangeEvent evt)
 		{
-		updateUI();
+		repaint();
 		}
 	}

commit c4a130db77fac82932283400aa44b055af35902d

Fix Image Preview Jittering Regression Setting the size in 91be58d31c59ca8d656b6f1deba0bdc67eef1730 was inconsistent with how we've always used the preferred size. The simple answer was to instead revalidate when the preffered size changes, such as by zooming, which causes the component to be layed out again using its preferred size. This actually fixes an issue in the sprite frame playback where it was jittery and you could not drag the sprite origin with the mouse while animation was playing. That's because the setSize call was causing the component to end up in the top-left of the scroll viewport instead of the center. I also abstracted the resizeAndRepaint logic since it's the same JTable uses internally and put it into the abstract image preview parent class so it doesn't keep being duplicated.

org/lateralgm/components/visual/AbstractImagePreview.java
@@ -47,6 +47,21 @@ public abstract class AbstractImagePreview extends JLabel
 		return new Dimension(bi.getWidth(),bi.getHeight());
 		}
 
+	/**
+	 * Refreshes the image preview by laying it out to its
+	 * preferred dimensions and then repainting it. Follows the
+	 * same conventions that {@code JTable.resizeAndRepaint} and
+	 * other Swing components do internally. It is not to be
+	 * used when the size hasn't actually changed, e.g
+	 * transparency pixel removed. In that case, only a simple
+	 * repaint is all that is needed.
+	 */
+	protected void resizeAndRepaint()
+		{
+		revalidate(); // size change, do layout
+		repaint(); // repaint it with new size
+		}
+
 	@Deprecated
 	public void setIcon(Icon ico)
 		{

org/lateralgm/components/visual/BackgroundPreview.java
@@ -47,10 +47,6 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 		b.reference.updateSource.addListener(this);
 		}
 
-	@Override public void setSize(Dimension d) {
-		super.setSize(d);
-	}
-
 	protected BufferedImage getTransparentImage()
 		{
 		if (background == null) return null;
@@ -163,10 +159,7 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	protected BufferedImage getImage()
@@ -177,10 +170,7 @@ public class BackgroundPreview extends AbstractImagePreview implements UpdateLis
 	public void updated(UpdateEvent e)
 		{
 		// new image may be new size
-		// therefore, do layout again
-		revalidate();
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	private class BackgroundPropertyListener extends PropertyUpdateListener<PBackground>

org/lateralgm/components/visual/SubimagePreview.java
@@ -181,19 +181,14 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 	public void setZoom(double nzoom)
 		{
 		zoom = nzoom;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	public void setIndex(int i)
 		{
 		subIndex = i;
-		// size change automatically revalidates
-		this.setSize(this.getPreferredSize());
-		// paint it again too
-		repaint();
+		// we allow varying subimage size
+		this.resizeAndRepaint();
 		}
 
 	public int getIndex()
@@ -203,14 +198,14 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 
 	public void setShowBbox(boolean show)
 		{
-		if (show != showBbox) repaint();
-		showBbox = show;
+		if (show != showBbox) repaint(); // << posts event
+		showBbox = show; // << repaint event happens later
 		}
 
 	public void setShowOrigin(boolean show)
 		{
-		if (show != showOrigin) repaint();
-		showOrigin = show;
+		if (show != showOrigin) repaint(); // << posts event
+		showOrigin = show; // << repaint event happens later
 		}
 
 	protected void processMouseEvent(MouseEvent e)
@@ -263,10 +258,7 @@ public class SubimagePreview extends AbstractImagePreview implements UpdateListe
 	public void updated(UpdateEvent e)
 		{
 		// new image may be new size
-		// therefore, do layout again
-		revalidate();
-		// paint it again too
-		repaint();
+		this.resizeAndRepaint();
 		}
 
 	private class SpritePropertyListener extends PropertyUpdateListener<PSprite>

org/lateralgm/main/LGM.java
@@ -133,7 +133,7 @@ import com.sun.imageio.plugins.wbmp.WBMPImageReaderSpi;
 
 public final class LGM
 	{
-	public static final String version = "1.8.167"; //$NON-NLS-1$
+	public static final String version = "1.8.168"; //$NON-NLS-1$
 
 	// TODO: This list holds the class loader for any loaded plugins which should be
 	// cleaned up and closed when the application closes.