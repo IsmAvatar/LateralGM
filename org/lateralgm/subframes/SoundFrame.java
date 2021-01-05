00fcdf627aaf81d6dff222e631962a0fb024d81d
unknown <robertbcolton@gmail.com>
2021-01-05 08:29:44 -0500

    Attempt Pulse Audio IllegalStateException Patch Even though DataLine is not supposed to throw an exception on microsecond position, it does under OpenJDK with IcedTea Pulse audio. Since the sound doesn't have a position when it's not open, we can try this patch to get around it.

--- org/lateralgm/subframes/SoundFrame.java
+++ org/lateralgm/subframes/SoundFrame.java
@@ -266,7 +266,9 @@ public class SoundFrame extends InstantiableResourceFrame<Sound,PSound>
 		@Override
 		public long getPosition()
 			{
-			return (clip == null) ? 0 : clip.getMicrosecondPosition();
+			// Pulse audio in OpenJDK has a bug that requires us to check that the line
+			// is open, even getMicrosecondPosition is not supposed to throw an exception
+			return (clip == null || !clip.isOpen()) ? 0 : clip.getMicrosecondPosition();
 			}
 
 		@Override
